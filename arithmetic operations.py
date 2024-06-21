5775fdf085df51cb39b6e8209c464cdae857fc69
vaishnavi15 <150790626+vaishnavi150505@users.noreply.github.com>
2024-06-21 21:02:41 +0530

    Create arithmetic operations.py

---
+++ arithmetic operations.py
@@ -0,0 +1,13 @@
+# Addition
+a = 5
+b = 3
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division
+print("Division:", a / b)
