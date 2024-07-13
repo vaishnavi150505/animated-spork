52592eb147ca3d7f09736912614f7c5a505c08e7
vaishnavi15 <150790626+vaishnavi150505@users.noreply.github.com>
2024-07-13 21:42:39 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+# Accessing elements
+print(my_list[0])
+
+# Adding elements
+my_list.append(6)
+print(my_list)
+
+# Removing elements
+my_list.remove(3)
+print(my_list)
